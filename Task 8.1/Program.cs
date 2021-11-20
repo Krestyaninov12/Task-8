d17bf70e8ecdd566c88c98b9ad324530e870f1cd
Krestyaninov12 <kreda@pnpe.ru>
2021-11-21 01:13:14 +0300

    Добавьте файлы проекта.

---
+++ Task 8.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8._1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sum = 0;
+            StreamWriter writer = new StreamWriter("C:\\Users\\kreda\\Desktop\\Test\\test.txt"); 
+            Random r = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                int number = r.Next(1, 10);
+                writer.Write(number + ",");
+            }
+            writer.Close();
+
+            StreamReader reader = new StreamReader("C:\\Users\\kreda\\Desktop\\Test\\test.txt");
+            string text = reader.ReadToEnd();
+            reader.Close();
+            string[] count = text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < 10; j++)
+            { sum += Convert.ToInt32(count[j]); }
+           
+            Console.WriteLine(sum);
+            Console.ReadKey();
+          }          
+    }
+    }
+
---
+++ Task 8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            var files = Directory.GetFiles("C:\\Users\\kreda\\Desktop\\Test", "*.*", SearchOption.AllDirectories);
+            var dirs = Directory.GetDirectories("C:\\Users\\kreda\\Desktop\\Test", "*.*", SearchOption.AllDirectories);
+                       
+            Console.WriteLine("По заданному адресу расположены следующие папки:");
+            foreach (string j in dirs)
+                {
+                    Console.WriteLine(j);
+                }
+            Console.WriteLine();
+            Console.WriteLine("По заданному адресу расположены следующие файлы");
+              foreach (string i in files)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }           
+            
+        
+    }
+}
